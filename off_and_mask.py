c0b09d5b5d80189025aff2b43a1d08684d7ff500
Nathanaël Sensfelder <spamshield+github.com@segfault.tech>
2025-08-18 17:37:36 +0200

    Adds gren.vim

---
+++ off_and_mask.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+
+import sys
+
+def myhex (base_num: int):
+	base_hex = hex(base_num)
+	return base_hex[0:2] + base_hex[2:].upper()
+
+def off_and_mask (start: int, end: int):
+	print("Offset: " + myhex(start) + "U")
+	print("Mask: " + myhex(((1 << (end + 1)) - 1) & ~((1 << (start)) - 1)) + "U")
+
+if (len(sys.argv) != 3):
+	print("Expects arguments: START END")
+	exit(-1)
+
+off_and_mask(int(sys.argv[1]), int(sys.argv[2]))
+
