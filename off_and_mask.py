76675a2fccf4c8dd2090cdb921c567fa99a64c93
Nathanaël Sensfelder <spamshield+github.com@segfault.tech>
2025-08-20 14:26:04 +0200

    Highlight overly-long idents in C.

--- off_and_mask.py
+++ off_and_mask.py
@@ -7,6 +7,11 @@ def myhex (base_num: int):
 	return base_hex[0:2] + base_hex[2:].upper()
 
 def off_and_mask (start: int, end: int):
+	if (start > end):
+		oopsy = end
+		end = start
+		start = oopsy
+	print("Bit(s): " + str(start) + "-" + str(end))
 	print("Offset: " + myhex(start) + "U")
 	print("Mask: " + myhex(((1 << (end + 1)) - 1) & ~((1 << (start)) - 1)) + "U")
 
